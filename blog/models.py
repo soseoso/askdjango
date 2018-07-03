f6a960f8f6cac163efea0759a64c51d30acc081d
soseoso <soseoso@naver.com>
2018-07-03 21:10:33 +0900

    blog.models.Post 모델에 옵션 필드인 lnglat/tags 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -1,20 +1,20 @@
+import re
 from django.db import models
+from django.forms import ValidationError
+
+def lnglat_validator(value):
+	if not re.match(r'^([+-]\d+\.?\d*), (\d+\.?\d*)$', value):
+		raise ValidationError('Invalid LngLat Type')
 
 class Post(models.Model):
-	# 길이 제한이 있는 문자열
-	title = models.CharField(max_length=100,
-		choices = (
-			# ('실제 저장될 값', 'UI에 보여질 레이블')
-			('해의 등불', '제목1 레이블'),
-			('제목2', '제목2 레이블'),
-			('제목3', '제목3 레이블'),
-		),
-		verbose_name='제목')
-	# 길이 제한이 없는 문자열
+	title = models.CharField(max_length=100, verbose_name='제목',
+		help_text='포스팅 제목을 입력해주세요. 최대 100자 내외.')
 	content = models.TextField(verbose_name="내용")
-	# auto_now_add=True 옵션 : Post 레코드 하나가 
-	# '최초'로 생성/저장될 때의 일시가 저장될 것.
+	# blank option 활용
+	tags = models.CharField(max_length=100, blank=True)
+	lnglat = models.CharField(max_length=50, blank=True,
+		# 함수를 호출하는 것이 아니라 함수 자체를 넘겼다. 
+		validators=[lnglat_validator],
+		help_text='경도/위도 포맷으로 입력')
 	created_at = models.DateTimeField(auto_now_add=True)
-	# auto_now=True 옵션 : 
-	# 해당 레코드가 갱신될 때마다의 일시가 저장된다.
 	updated_at = models.DateTimeField(auto_now=True)
