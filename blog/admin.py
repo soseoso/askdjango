f7a067697f1d4d6c038357cc3e7efc4380d2250a
soseoso <soseoso@naver.com>
2018-07-05 13:32:19 +0900

    blog.admin.PostAdmin에 list_display 커스텀

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	list_display = ['id', 'title', 'content_size', 'created_at', 'updated_at']
+
+	def content_size(self, post):
+		return mark_safe('<strong>{}</strong>글자'.format(len(post.content)))
+	content_size.short_description = '글자 수'
+	# content_size.allow_tags = True 이것은 사라진 옵션..
+
+	
