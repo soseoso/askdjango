450af832d9f0c19fe494dc7e07725d6ea4dd8db1
soseoso <soseoso@naver.com>
2018-07-08 10:51:35 +0900

    blog.views.post_list º信¼­Æ÷½ºƮ ¸ñ·Ï ±¸Çö

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-	return render(request, 'blog/post_list.html')
+	qs = Post.objects.all()
+	# 아직 이 시점에는 DB 엑세스가 이뤄지지 않았다
+
+	return render(request, 'blog/post_list.html', {
+			'post_list': qs,  # 쿼리셋을 인자로 넘겨줌
+		})
